aa233bdd674b127e3aca22685ceafbec7a7ce66e
wuchuixu <364191418@qq.com>
2022-09-09 23:56:35 +0800

    fix：富文本样式

--- lib/src/common/rich_text/rich_txt_page.dart
+++ lib/src/common/rich_text/rich_txt_page.dart
@@ -24,8 +24,25 @@ class _RichTxtPageState extends State<RichTxtPage> {
           ),
           body: SingleChildScrollView(
             physics: const ClampingScrollPhysics(),
-            padding: EdgeInsets.all(10.w),
-            child: Html(data: c.content),
+            padding: EdgeInsets.all(20.w),
+            child: Html(
+              data: c.content
+                  .replaceAll('<br/>', '<div style="height: 17px"> </div>'),
+              style: {
+                'body': Style(
+                  margin: EdgeInsets.zero,
+                  lineHeight: LineHeight.number(1.3),
+                  fontSize: FontSize.medium,
+                ),
+                'p': Style(
+                  margin: EdgeInsets.zero,
+                ),
+                'br': Style(
+                  height: 0,
+                  margin: EdgeInsets.zero,
+                ),
+              },
+            ),
           ),
         );
       },
